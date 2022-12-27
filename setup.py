fe505f05cb95d7e8a7363cddb62dc6de6eeca2c3
M41den <38406005+cradio@users.noreply.github.com>
2022-12-27 22:12:44 +0300

    E

--- setup.py
+++ setup.py
@@ -2,13 +2,19 @@ from setuptools import setup
 
 setup(
     name='wheezeloader',
-    version='0.1.0',
+    version='0.2.0',
     description='Mom look I can build stuff',
     url='https://github.com/cradio/WheezerLoader',
     author='M41den',
     author_email='no-no-no',
     license='IDK',
-    packages=['wheezeloader'],
+    packages=[
+        'wheezeloader',
+        'wheezeloader.deezloader',
+        'wheezeloader.libutils',
+        'wheezeloader.models',
+        'wheezeloader.spotloader'
+    ],
     install_requires=[
         'fastapi',
         'librespot',
